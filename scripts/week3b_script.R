526d7f6d0fdeb0b853a6f4702c8a9e406ed07c61
Lakshman090 <mknvlakshman@gmail.com>
2026-03-02 14:03:52 +0530

    Initial commit

---
+++ scripts/week3b_script.R
@@ -0,0 +1,18 @@
+# DV Lab – Week 03b
+# Name:K.Sarveswara Rao
+# Roll No:23A91A6187
+library(ggplot2)
+
+# Load dataset
+data("airquality")
+
+# Remove missing values
+df <- na.omit(airquality)
+
+# Histogram of Ozone concentration
+ggplot(df, aes(x = Ozone)) +
+  geom_histogram(binwidth = 10, fill = "steelblue", color = "black") +
+  labs(title = "Distribution of Ozone Concentration",
+       x = "Ozone (ppb)",
+       y = "Frequency") +
+  theme_minimal()
